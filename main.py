032687bfec6bc517784de5f9940a56f3debfb6ca
Minigrim0 <grimauflorent@gmail.com>
2021-10-07 10:09:47 +0200

    [Add] basic parsing + command

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import discord
+from models.client import MaxVUBot
+
+
+API_KEY = os.environ.get("API_KEY", "")
+if API_KEY == "":
+    raise RuntimeError("You must specify an API key")
+
+client = UrBot.getInstance()
+
+intents = discord.Intents.default()
+intents.members = True
+
+client.run(API_KEY)
---
+++ models/client.py
@@ -0,0 +1,45 @@
+import discord
+
+from discord_slash import SlashCommand
+
+from src.parser import parse_command
+from models.command_registry import CommandRegistry
+
+
+class MaxVUBot(discord.Client):
+    """MaxVUBot's client"""
+
+    instance = None
+
+    @staticmethod
+    def getInstance():
+        """Singleton Pattern"""
+        if MaxVUBot.instance is None:
+            MaxVUBot()
+        return MaxVUBot.instance
+
+    def __init__(self):
+        if MaxVUBot.instance is not None:
+            raise RuntimeError(
+                f"Trying to instanciate a second object of {__class__}")
+        MaxVUBot.instance = self
+        super().__init__()
+
+        import src.commands  # NOQA
+
+        self.slash = SlashCommand(self, sync_commands=True)
+        self.registry = CommandRegistry.getInstance()
+
+    async def on_ready(self):
+        """This is called when the connection to disord's API has succeed"""
+        print(f"Logged on as {self.user}!")
+
+    async def on_message(self, message: discord.Message):
+        """Called when a message is sent womewhere the bot can access
+        Args:
+            message (discord.Message): The message with its metadata
+        """
+        if message.content and message.content[0] == "$":
+            params = await parse_command(message)
+            command = self.registry.get(params["command"]["command"])
+            await command(**params)
