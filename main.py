c7e6d0091d68ca656aa0aec861e69362ba1a9362
Minigrim0 <grimauflorent@gmail.com>
2021-11-11 14:34:15 +0100

    [Upd] API key + logging format

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-import os
-import discord
+import json
+import logging
+
+import coloredlogs
+from hereby import Here
+
 from models.client import MaxVUBot
 
+if __name__ == "__main__":
+    here = Here(__file__)
 
-API_KEY = os.environ.get("API_KEY", "")
-if API_KEY == "":
-    raise RuntimeError("You must specify an API key")
+    coloredlogs.install()
+    logging.basicConfig(
+        level=logging.DEBUG,
+    )
 
-client = UrBot.getInstance()
+    with open(here.abspath(".env.json")) as key:
+        API_KEY = json.load(key).get("API_KEY", None)
 
-intents = discord.Intents.default()
-intents.members = True
+    if API_KEY is None:
+        logging.error("No API key found ! quitting")
+    else:
+        client = MaxVUBot.get_instance()
 
-client.run(API_KEY)
+        client.run(API_KEY)
